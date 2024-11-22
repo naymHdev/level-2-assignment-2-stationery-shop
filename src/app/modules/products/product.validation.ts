6e474f5a362cc56ac82fca87c22a41d92242bdf5
MacBook Pro <naym100m@gmail.com>
2024-11-22 11:36:48 +0600

    [created] product validation schema

---
+++ src/app/modules/products/product.validation.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+
+// Zod schema for core validations
+
+const productValidationSchema = z.object({
+  id: z.string({
+    required_error: 'ID is must be required',
+  }),
+  name: z.string({
+    required_error: 'Product name must be required',
+  }),
+  brand: z.string({
+    required_error: 'Product brand name must be required',
+  }),
+  price: z
+    .number({
+      required_error: 'Product price must be required',
+    })
+    .max(0, { message: 'Price must be greater than or equal to 0' }),
+  category: z.enum(
+    ['Writing', 'Office Supplies', 'Art Supplies', 'Educational', 'Technology'],
+    { required_error: 'Category must be required' },
+  ),
+  description: z.string({
+    required_error: 'Description must be required',
+  }),
+  quantity: z
+    .number({
+      required_error: 'Quantity is required',
+    })
+    .min(0, { message: 'Quantity must be greater than or equal to 0' }),
+  inStock: z.boolean({
+    required_error: 'inStock Filed is required',
+  }),
+});
+
+export default productValidationSchema;

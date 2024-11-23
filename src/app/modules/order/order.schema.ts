e905a256dccb705e7a529836f89d7313ca4c24ce
MacBook Pro <naym100m@gmail.com>
2024-11-23 10:10:03 +0600

    [created]  a order model and pre-save middleware to calculate the product total price and find product order by ID - functionality

---
+++ src/app/modules/order/order.schema.ts
@@ -0,0 +1,58 @@
+import mongoose, { Document, model, Schema } from 'mongoose';
+
+//  Create a product order Interface
+interface IOrder extends Document {
+  email: string;
+  product: mongoose.Schema.Types.ObjectId;
+  quantity: number;
+  totalPrice: number;
+}
+
+//  Create a order Schema
+const OrderSchema: Schema = new Schema<IOrder>(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required for parching product'],
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        'Please enter a valid email',
+      ],
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'StationeryProductModel',
+      required: [true, 'Product is required'],
+    },
+    quantity: {
+      type: Number,
+      required: [true, 'Product Quantity is required'],
+      min: [1, 'Quantity must be at least 1'],
+    },
+    totalPrice: {
+      type: Number,
+      required: [true, 'Product total price is required'],
+      min: [0, "Total price can't be negative"],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// pre-save middleware to calculate the product total price and find product order by ID
+OrderSchema.pre('save', async function (next) {
+  if (this.isModified('quantity') && this.isModified('product')) return next();
+
+  const product = await mongoose
+    .model('StationeryProductModel')
+    .findById(this.product);
+  if (!product) throw new Error('Product not found');
+
+  this.totalPrice = product.price * this.quantity;
+  next();
+});
+
+const OrderModel = model<IOrder>('Orders', OrderSchema);
+
+export default OrderModel;
